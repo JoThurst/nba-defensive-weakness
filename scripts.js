eab08bd487d0498db665c0a98e47112f43409e4a
Jordan Thurston <jothurst@gmail.com>
2024-04-04 23:42:07 -0400

    Storing row data after inital parse instead of creating table to avoid duplicate rows on different time series

--- scripts.js
+++ scripts.js
@@ -58,21 +58,74 @@ function generateTablesHtml(teamsData) {
     document.getElementById('tables-container').innerHTML = tablesHtml;
 }
 
+function containsStatRowObj(arrayObj, string){
+    for (let i = 0; i < arrayObj.length; i++) {
+        //console.log(arrayObj[i][0], " " , string)
+        if (arrayObj[i][0] === string) {
+            console.log("Match Found");
+            return i; // Match found
+        }
+    }
+    return false;
+}
+
 function generateTableRows(timeSplits) {
+    let tableRowsObj = [];
     let tableRows = '';
     Object.keys(timeSplits).forEach(timeSplit => {
         Object.keys(timeSplits[timeSplit]).forEach(statistic => {
-            tableRows += `
-                <tr>
-                    <td>${statistic}</td>
-                    <td>${timeSplits[timeSplit][statistic]['GC-0'] || ''}</td>
-                    <td>${timeSplits[timeSplit][statistic]['GC-7'] || ''}</td>
-                    <td>${timeSplits[timeSplit][statistic]['GC-15'] || ''}</td>
-                    <td>${timeSplits[timeSplit][statistic]['GC-30'] || ''}</td>
-                </tr>
-            `;
+            let statrow = [statistic, null, null, null, null, null]
+            index = containsStatRowObj(tableRowsObj,statistic)
+            if(index === false){
+                console.log("False Contains Keeping New Statrow", statrow)
+                
+            }else{
+                console.log("True Grabbing Stored Statrow", index)
+                statrow = tableRowsObj[index]
+                console.log(statrow)
+            }
+            
+           
+            if(tableRowsObj.includes)
+            tableRows += '<tr> <td>' + statistic + '</td>';
+            timeSplitArray = ["GC-0","GC-7","GC-15","GC-30"];
+            for (let index = 0; index < timeSplitArray.length; index++) {
+                if(typeof timeSplits[timeSplitArray[index]] === 'undefined' || typeof timeSplits[timeSplitArray[index]] === undefined){
+                    console.log('Undefined.. SKIP');
+
+                }else{
+                    if(timeSplits[timeSplitArray[index]] !== timeSplits[timeSplit]){
+
+                    }else{
+                        console.log("Defined Statistic ", timeSplits[timeSplitArray[index]][statistic], statistic)
+
+                        statrow[index+1] = timeSplits[timeSplitArray[index]][statistic]
+                    }
+
+                }
+                
+            }
+            console.log("Statrow After Loop", statrow)
+            if(index === false){
+                tableRowsObj.push(statrow);
+            }else{
+                tableRowsObj[index] = statrow
+            }
+            
+
+            // tableRows += `
+            //     <tr>
+            //         <td>${statistic}</td>
+            //         <td>${timeSplits['GC-0'][statistic] || ''}</td>
+            //         <td>${timeSplits['GC-7'][statistic] || ''}</td>
+            //         <td>${timeSplits['GC-15'][statistic] || ''}</td>
+            //         <td>${timeSplits['GC-30'][statistic] || ''}</td>
+            //     </tr>
+            // `;
         });
     });
+
+    console.log(tableRowsObj);
     return tableRows;
 }
 
